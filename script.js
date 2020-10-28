162244dfb839cd56b783d0557463ebc018a3a388
Kasey Cushing <kasey.cushing@gmail.com>
2020-10-27 21:37:51 -0400

    add API keys for NYT, penguin random house publishing, and openlibrary

---
+++ script.js
@@ -0,0 +1,10 @@
+/*API keys for future reference
+let keyNYT = FDa4Sil51Sz6BlQAjD6vGlH9WypDRJug
+
+let key bookPenguinPub = v2uy7a4bb8m4eazgwg9qm3np
+//need to do more research on using penguin house api
+
+bookCovers does not need personal API key
+bookCoversURL = http://covers.openlibrary.org/b/$key/$value-$size.jpg
+where key, value, and size must be specified
+*/
