b7168ec783a6204c738d25cb333f932d27695c9d
Imprudent Friend <ImprudentFriend@gmail.com>
2020-10-29 21:32:22 -0400

    added weather api

--- script.js
+++ script.js
@@ -13,4 +13,17 @@ where key, value, and size must be specified
 let keyOMD = 3e7d0c9b
 
 api key for LastFM:
-let keyLastFM = eabf51c045a9b6cc69cda73e4364ab9d
+let keyLastFM = eabf51c045a9b6cc69cda73e4364ab9d*/
+
+/* API key for Visual Crossing Weather
+//let APIKeyVC = "HFS2SKT2Y8KUVY6FAG6KJAZ6";
+//New York Query
+let queryURLforNY = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?aggregateHours=24&combinationMethod=aggregate&startDateTime=2020-10-22T00%3A00%3A00&endDateTime=2020-10-22T00%3A00%3A00&maxStations=-1&maxDistance=-1&contentType=json&unitGroup=us&locationMode=single&key=HFS2SKT2Y8KUVY6FAG6KJAZ6S&dataElements=default&locations=New%20York%2C%20New%20York"
+
+$.ajax({
+url: queryURLforNY,
+method: "GET"
+})
+.then(function(response) {
+    console.log(response);
+});*/
