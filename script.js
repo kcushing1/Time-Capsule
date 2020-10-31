82c02e1bc9ff698a04230c7eef722d22d59c9c68
ala decoste <alshareefialaa@gmail.com>
2020-10-31 10:29:36 -0400

    Used Materalize instead of getBootstrab.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ where key, value, and size must be specified
 
 function parallax_height() {
     var scroll_top = $(this).scrollTop();
-    var sample_section_top = $(".sample-section").offset().top;
+    var sample_section_top = $(".sample-section").offset();
     var header_height = $(".sample-header-section").outerHeight();
     $(".sample-section").css({ "margin-top": header_height });
     $(".sample-header").css({ height: header_height - scroll_top });
@@ -25,27 +25,9 @@ function parallax_height() {
   });
 
 
-//  Calendar code: https://bit.ly/2TBnpdj
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.datepicker');
+  let options = {format: 'yyyy-mm-dd'};
+  instances = M.Datepicker.init(elems, options);
+}); 
 
-function setDatepicker(_this) { 
-  
-    /* Get the parent class name so we  
-        can show date picker */ 
-    let className = $(_this).parent() 
-        .parent().parent().attr('class'); 
-
-    // Remove space and add '.' 
-    let removeSpace = className.replace(' ', '.'); 
-
-    // jQuery class selector 
-    $("." + removeSpace).datepicker({ 
-        format: "dd/mm/yyyy", 
-        yearRange: [2000,2020],
-        // Positioning where the calendar is placed 
-        orientation: "bottom auto", 
-        // Calendar closes when cursor is  
-        // clicked outside the calendar 
-        autoclose: true, 
-        showOnFocus: "false" 
-    }); 
-} 
